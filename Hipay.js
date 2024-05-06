87863b87b74018cf9606d53cb6148579e8fc4fee
sunguodong <952519440@qq.com>
2024-05-06 08:50:48 +0800

    Add files via upload
    海底捞自动领取

---
+++ Hipay.js
@@ -0,0 +1,205 @@
+// 海底捞签到
+/**
+ * 海底捞小程序签到
+ * cron 0 8 * * *  Hi.js
+ * 
+ * ========= 青龙--配置文件 ===========
+ * # 项目名称
+ * export hdl_token='token @ token'
+ * 
+ * 多账号用 换行 或 @ 分割
+ * 抓包 https://superapp-public.kiwa-tech.com/activity/wxapp , 找到 _haidilao_app_token 即可
+ * ====================================
+ *   
+ */
+
+ let { delay, getCurrentData, getDataList, sendEmail, getCurrentTime, Env, checkEnv, SendMsg } = require('./common.js')
+ const $ = new Env("海底捞兑换");
+ const ckName = "hdl_token";
+ let allDataList = [];
+ var request = require("request");
+ function getActivityId (token) {
+   return new Promise((resolve, reject) => {
+     request({
+       url: 'https://superapp-public.kiwa-tech.com/activity/wxapp/signin/querySite',
+       method: "POST",
+       json: true,
+       headers: {
+         'Host': 'superapp-public.kiwa-tech.com',
+         'deviceid': null,
+         'accept': 'application/json, text/plain, */*',
+         'content-type': 'application/json',
+         'user-agent': 'Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160027 MMWEBSDK/20231105 MMWEBID/1420 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wx1ddeb67115f30d1a',
+         'reqtype': 'APPH5',
+         '_haidilao_app_token': token,
+         'origin': 'https://superapp-public.kiwa-tech.com',
+         'x-requested-with': 'com.tencent.mm',
+         'sec-fetch-site': 'same-origin',
+         'sec-fetch-mode': 'cors',
+         'sec-fetch-dest': 'empty',
+         'referer': `https://superapp-public.kiwa-tech.com/app-sign-in/?SignInToken=${token}&source=MiniApp`
+       },
+       body: {"signinSource":"MiniApp"}
+     }, function(error, response, body) {
+       if (body && body.data && body.data.convertUrl) {
+         let arr = body.data.convertUrl.split('/')
+         let activityId = arr[arr.length - 1]
+         resolve(activityId)
+       } else {
+        resolve('')
+       }
+     })
+   })
+ }
+
+ function queryList (token, activityId) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'https://superapp-public.kiwa-tech.com/activity/wxapp/exchange/queryCommodityList',
+      method: "POST",
+      json: true,
+      headers: {
+        'Host': 'superapp-public.kiwa-tech.com',
+        'deviceid': null,
+        'accept': 'application/json, text/plain, */*',
+        'content-type': 'application/json',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160027 MMWEBSDK/20231105 MMWEBID/1420 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wx1ddeb67115f30d1a',
+        'reqtype': 'APPH5',
+        '_haidilao_app_token': token,
+        'origin': 'https://superapp-public.kiwa-tech.com',
+        'x-requested-with': 'com.tencent.mm',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': `https://superapp-public.kiwa-tech.com/app-sign-in/?SignInToken=${token}&source=MiniApp`
+      },
+      body: {"exchangeActivityId": activityId}
+    }, function(error, response, body) {
+      if (body && body.data && body.data.commodityDetailList) {
+        resolve(body.data.commodityDetailList)
+      } else {
+       resolve([])
+      }
+    })
+  })
+}
+ 
+ // 查询签到周期
+ function pay (token, activityId ,commodityId) {
+   return new Promise((resolve, reject) => {
+     request({
+       url: 'https://superapp-public.kiwa-tech.com/activity/wxapp/exchange/exchangeCommodity',
+       method: "POST",
+       json: true,
+       headers: {
+         'Host': 'superapp-public.kiwa-tech.com',
+         'deviceid': null,
+         'accept': 'application/json, text/plain, */*',
+         'content-type': 'application/json',
+         'user-agent': 'Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160027 MMWEBSDK/20231105 MMWEBID/1420 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wx1ddeb67115f30d1a',
+         'reqtype': 'APPH5',
+         '_haidilao_app_token': token,
+         'origin': 'https://superapp-public.kiwa-tech.com',
+         'x-requested-with': 'com.tencent.mm',
+         'sec-fetch-site': 'same-origin',
+         'sec-fetch-mode': 'cors',
+         'sec-fetch-dest': 'empty',
+         'referer': `https://servicewechat.com/wx1ddeb67115f30d1a/131/page-frame.html`
+       },
+       body: {"commodityId": commodityId, "exchangeActivityId": activityId, "exchangeResource": ''}
+     }, function(error, response, body) {
+      console.log(body)
+       if (body.code == 'ok' && body.data) {
+         resolve(body.data.validatecodedesc || body.data.validateCodeDesc)
+       } else {
+         resolve('')
+       }
+     })
+     
+   })
+ }
+ 
+ 
+ // 查询签到周期
+ function queryFragment (token) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'https://superapp-public.kiwa-tech.com/activity/wxapp/signin/queryFragment',
+      method: "POST",
+      json: true,
+      headers: {
+        'Host': 'superapp-public.kiwa-tech.com',
+        'deviceid': null,
+        'accept': 'application/json, text/plain, */*',
+        'content-type': 'application/json',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 9; MI 6 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160027 MMWEBSDK/20231105 MMWEBID/1420 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 miniProgram/wx1ddeb67115f30d1a',
+        'reqtype': 'APPH5',
+        '_haidilao_app_token': token,
+        'origin': 'https://superapp-public.kiwa-tech.com',
+        'x-requested-with': 'com.tencent.mm',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': `https://superapp-public.kiwa-tech.com/app-sign-in/?SignInToken=${token}&source=MiniApp`
+      },
+      body: {"signinSource":"MiniApp"}
+    }, function(error, response, body) {
+      if (body.code == 'ok') {
+        console.log(body)
+          console.log('碎片过期日期:' + body.data.expireDate.substring(0,10))
+          if (body.data.expireDate.substring(0,10) == body.data.today.substring(0,10)) {
+              resolve(true)
+          } else {
+           resolve(false)
+          }
+      } else {
+        resolve(false)
+      }
+    })
+    
+  })
+}
+ // 方法主体
+let autoSign = async () => {
+  let msg = '';
+  if (allDataList.length > 0) {
+    if (!await queryFragment(allDataList[0])) {
+      console.log('未到兑换日期,暂不兑换')
+      return
+    }
+  } else {
+    return
+  }
+  await delay() // 延迟
+  for(let key = 0; key < allDataList.length; key++) {
+    let activityId = await getActivityId(allDataList[key])
+    await delay() // 延迟
+    let shopList = await queryList(allDataList[key], activityId)
+    await delay() // 延迟
+    console.log(`-------------第${key +1 }个账号开始尝试兑换----------`)
+    if (shopList.length > 0) {
+      for(let i = 0; i < shopList.length; i++) {
+        let item = shopList[i]
+        if (item.commodityName && item.commodityName.indexOf('海底捞新一代番茄火锅') > -1) {
+          // 兑换 海底捞新一代番茄火锅
+          msg = await pay(allDataList[key], activityId, item.id)
+          break;
+        }
+      }
+    }
+    console.log('兑换结果:' + msg)
+  }
+}
+ 
+ //固定的调用方法,无需修改
+ !(async () => {
+   if (!(await checkEnv($, ckName))) return;
+   allDataList = await checkEnv($, ckName)
+   if (allDataList.length > 0) {
+       await autoSign();
+   }
+  //  console.log(msg)
+ })()
+   .catch((e) => console.log(e))
+   .finally(() => $.done());
+ 
---
+++ common.js
@@ -0,0 +1,133 @@
+//  公共方法
+var request = require("request");
+// var {SMTPClient} = require("emailjs");
+var fs = require('fs'); //文件模块
+
+let delayTime = 10000 // 延迟时间
+
+let envSplitor = ["@", "\n"]; //多账号分隔符
+const Notify = 0;		 //0为关闭通知,1为打开通知,默认为1
+
+function delay(time) {
+  return new Promise((resolve, reject) => {
+    time = time ? (time + Math.floor(Math.random() * time)) : (delayTime + Math.floor(Math.random() * delayTime))
+    setTimeout(resolve, time)
+  })
+}
+function getCurrentData() {
+  let date = new Date();
+  return date.getFullYear() + '' + (date.getMonth() + 1) + date.getDate()
+}
+function getCurrentDataStr() {
+  let date = new Date();
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  month = month > 9 ? month : '0' + month
+  let day = date.getDate() > 9 ?  date.getDate() : '0' + date.getDate()
+  return year + '' + month + day
+}
+function getCurrentTime() {
+  let date = new Date();
+  return '[' + date.getFullYear() + '-' + (date.getMonth() + 1)  + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ']:'
+}
+// 读取本地文件
+function getDataList(fileName = 'data') {
+  return new Promise((resolve, reject) => {
+    fs.readFile(`./${fileName}.json`, 'utf-8', function(err, data) {
+      if (err) {
+          console.log('文件读取失败');
+          resolve('')
+      } else {
+        resolve(JSON.parse(data))
+      }
+    });
+  })
+}
+
+// 发送邮件
+function sendEmail(text, subject='签到结果',emailList) {
+  let currentData = getCurrentData()
+  // var client = new SMTPClient({
+  //     user: "992007307@qq.com", // 你的QQ用户
+  //     password: "szqauyrbnrwpbcbd", // 这里是上面生成的授权码，不是QQ密码
+  //     host: "smtp.qq.com", // 主机，不改
+  //     ssl: true // 开启ssl
+  // });
+  //开始发送邮件
+  /*client.send({
+      text: text, //邮件内容
+      from: "992007307@qq.com", //你的邮箱号
+      to: "1696674002@qq.com", //发送给谁的
+      subject: subject //邮件主题
+  }, function (err, message) {
+    //回调函数
+    console.log(err || message);
+  });*/
+  // if (emailList.length > 0) {
+  //   for (let i =0;i < emailList.length; i++) {
+  //     //开始发送邮件
+  //     client.send({
+  //       text: text, //邮件内容
+  //       from: "992007307@qq.com", //你的邮箱号
+  //       to: emailList[i].email, //发送给谁的
+  //       subject: subject //邮件主题
+  //     }, function (err, message) {
+  //       //回调函数
+  //       console.log(err || message);
+  //     });
+  //   }
+  // }
+}
+// 变量检查与处理
+async function checkEnv($, ckName) {
+  let cookie = ($.isNode() ? process.env[ckName] : $.getdata(ckName)) || '';
+  let userList = []
+  if (cookie) {
+      let e = envSplitor[0];
+      for (let o of envSplitor)
+          if (cookie.indexOf(o) > -1) {
+              e = o;
+              break;
+          }
+      for (let n of cookie.split(e)) n && userList.push(n);
+      userCount = userList.length;
+  } else {
+      console.log("未找到CK");
+      return;
+  }
+  console.log(`共找到${userCount}个账号`)
+  return userList;
+}
+
+// 双平台log输出
+function DoubleLog($, data) {
+  if ($.isNode()) {
+      if (data) {
+          console.log(`${data}`);
+          msg += `\n${data}`
+      }
+  } else {
+      console.log(`${data}`);
+      msg += `\n${data}`
+  }
+}
+// 发送消息
+async function SendMsg($, message, upNotify = 0) {
+  if (!message) return;
+  if (Notify > 0 || upNotify > 0) {
+      if ($.isNode()) {
+          var notify = require("./sendNotify");
+          await notify.sendNotify($.name + message, message)
+      } else {
+          $.msg($.name, '', message)
+      }
+  } else {
+      console.log(message)
+  }
+}
+
+// 完整 Env
+function Env(t, e) { "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0); class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `🔔${this.name}, 开始!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), n = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(n, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); s && this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t, e = null) { const s = e ? new Date(e) : new Date; let i = { "M+": s.getMonth() + 1, "d+": s.getDate(), "H+": s.getHours(), "m+": s.getMinutes(), "s+": s.getSeconds(), "q+": Math.floor((s.getMonth() + 3) / 3), S: s.getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, (s.getFullYear() + "").substr(4 - RegExp.$1.length))); for (let e in i) new RegExp("(" + e + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? i[e] : ("00" + i[e]).substr(("" + i[e]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; if (this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))), !this.isMuteLog) { let t = ["", "==============📣系统通知📣=============="]; t.push(e), s && t.push(s), i && t.push(i), console.log(t.join("\n")), this.logs = this.logs.concat(t) } } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `❗️${this.name}, 错误!`, t.stack) : this.log("", `❗️${this.name}, 错误!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `🔔${this.name}, 结束! 🕛 ${s} 秒`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
+
+module.exports = { delay, getCurrentData, getCurrentDataStr, getDataList, sendEmail, getCurrentTime, Env, checkEnv, DoubleLog, SendMsg }
+// module.exports  = delay
